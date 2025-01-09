0ecb2b22a1e50d8f9b6dd345f6181d64f6677fda
abdullahau <abdullah.au@outlook.com>
2025-01-09 23:45:40 +0400

    update

--- 17_Letter_Combinations_of_a_Phone_Number.py
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -1,19 +1,58 @@
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 from collections import deque
-# def letterCombinations(digits: str) -> list[str]:
 
-# letter_map = {'2': ['a','b','c'], '3': ['d','e','f'], '4': ['g','h','i'], '5': ['j','k','l'], 
-#                 '6': ['m','n','o'], '7': ['p','q','r','s'], '8': ['t','u','v'], '9': ['w','x','y','z']}
-
-letter_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
-              '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+def letterCombinations(digits):
+    letter_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
+                '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}    
     
-def letterCombinations(digit):
-    letters = [letter_map[i] for i in digit]
-    iterator = deque(map(iter, letters))
-    while iterator:
-        yield next(iterator[0])
-        iterator.rotate(-1)
+    n = len(digits)
+    
+    # Result list to store all combinations
+    combinations = []    
+    if n==0: return combinations
+    
+    # Helper function for recursion
+    def backtrack(index, current_combination):
+        # Base Case (to stop recursion): If the current combination is complete, add it to the result
+        if index == n:
+            combinations.append("".join(current_combination))
+            return
+        
+        # Get the letters for the current digit
+        current_digit = digits[index]
+        for letter in letter_map[current_digit]:
+            # Add the letter to the current combination
+            current_combination.append(letter)
+            # Recurse for the next digit
+            backtrack(index + 1, current_combination)
+            # Backtrack: remove the last added letter
+            current_combination.pop() 
 
-d = letterCombinations(digit='23')
-print(next(d))
+    # Start the recursion
+    backtrack(0, deque())
+    return combinations       
+
+print(letterCombinations(digits='289'))
+
+# def letterCombinations(digits: str):
+#     letter_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', 
+#                 '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'} 
+
+#     n=len(digits)
+#     results=[]
+
+#     if n==0:return results
+
+#     def track(path:str,level:int):
+#         if level==n:
+#             results.append(path)
+#             return
+#         else:
+#             possible_letters = letter_map[digits[level]]
+#             for letter in possible_letters:
+#                 track(path+letter,level+1)
+
+#     track("",0)
+#     return results
+
+# print(letterCombinations(digits='289'))
